4b16f5f9e6a96a6ae220ca47b4fb78902302774b
spookyorange <korkmazcoltu@mail.ru>
2023-04-29 17:28:52 +0300

    remove sh added, install sh is more user friendly

--- install.sh
+++ install.sh
@@ -6,20 +6,25 @@ link=https://dl.discordapp.net/apps/linux/$version/discord-$version.tar.gz
 file=discord-$version.tar.gz
 dir=Discord
 
+echo "Installing Discord..."
+echo "System might ask for your password..."
+echo "It is needed to install Discord on your system, because it downloads it in your /opt folder."
+echo "Version: $version"
+
 # Delete from opt and usr if Discord is already installed
 if [ -d "/usr/bin/Discord" ]; then
     echo "Discord is already installed. Removing..."
-    rm -rf /usr/bin/Discord
+    sudo rm -rf /usr/bin/Discord
 fi
 
 if [ -d "/usr/share/discord/Discord" ]; then
     echo "Discord is already installed. Removing..."
-    rm -rf /usr/share/discord/Discord
+    sudo rm -rf /usr/share/discord/Discord
 fi
 
 if [ -d "/opt/Discord" ]; then
     echo "Discord is already installed. Removing..."
-    rm -rf /opt/Discord
+    sudo rm -rf /opt/Discord
 fi
 
 
@@ -32,9 +37,13 @@ curl -L $link -o $file
 echo "Extracting Discord..."
 tar -xvf $file
 
-# Install Discord
+# Change the code of the desktop so it will see the icon
+echo "Changing the code of the desktop so it will see the icon..."
+sed -i 's/Icon=discord/Icon=\/opt\/Discord\/discord.png/g' $dir/discord.desktop
 
+# Install Discord
 echo "Installing Discord..."
+echo "Moving Discord to /opt..., this might ask for your root password"
 sudo cp -r Discord /opt/Discord
 
 # Create desktop entry
@@ -54,11 +63,18 @@ echo "Creating symbolic link for users directory..."
 sudo ln -sf /opt/Discord /usr/bin/Discord
 
 # Cleanup
-
 echo "Cleaning up..."
 rm -rf $file
 rm -rf $dir
 
 echo "Done!"
+
+echo "Discord is now installed on your system, if it does not show up on the menu, restart your DE or log out and log back in."
+
+sleep 3
+
+echo "Have fun!"
+
+sleep 2
 
 exit 0
---
+++ remove.sh
@@ -0,0 +1,5 @@
+
+echo "Removing Discord..."
+sudo rm -rf /usr/bin/Discord
+sudo rm -rf /usr/share/discord/Discord
+sudo rm -rf /opt/Discord
